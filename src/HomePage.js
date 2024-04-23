15eb5e3fce7c78d57bfa701cd2813f0ed55e49dc
Luca Trianti <lukotty02@gmail.com>
2024-04-23 10:39:08 +0200

    Added HomePage and resources folder

---
+++ src/HomePage.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+
+function HomePage() {
+  return (
+    <div style={{backgroundColor: "#282c34", 
+                height:"100vh",
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "left",
+                justifyContent: "left",
+                color: "white", 
+                fontSize:"30px", 
+                fontWeight:"bold"}}>
+        <div>
+            <h1 style={{marginLeft:"50px"}}>Test test test</h1>
+        </div>
+    </div>
+  );
+}
+
+export default HomePage;
